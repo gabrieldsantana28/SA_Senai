38ad7722bdce9ad4166860b81c6c819323244656
Thauã Brandão <134437322+ThauaBrandao@users.noreply.github.com>
2024-10-23 20:28:50 -0300

    Css Atualizado 2
    Atualizando o css de outras páginas!
    Header 100% igual em todas as páginas

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=7">
-    <link rel="stylesheet" href="css/cadastroprodutos.css">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <title>Login</title>
     <style>
